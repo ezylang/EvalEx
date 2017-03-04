55fd40ad2e6fef4342931e1a7d9e5ad5486ad2e0
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-03-04 23:29:25 +0530

    Correct empty exception class

--- src/com/udojava/evalex/ExpressionException.java
+++ src/com/udojava/evalex/ExpressionException.java
@@ -7,5 +7,11 @@
 */
 package com.udojava.evalex;
 
-public class ExpressionException {
+public class ExpressionException extends RuntimeException {
+
+	private static final long serialVersionUID = 1118142866870779047L;
+
+	public ExpressionException(String message) {
+		super(message);
+	}
 }
