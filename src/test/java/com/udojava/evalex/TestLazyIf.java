beae447951a789fec11b1eebb7dc764033d63d96
Laurent YHUEL <lyhuel@asi.fr>
2019-08-13 10:16:36 +0200

    Fix warning + fix lazyifnumber getString() + improve tests name

--- src/test/java/com/udojava/evalex/TestLazyIf.java
+++ src/test/java/com/udojava/evalex/TestLazyIf.java
@@ -28,14 +28,14 @@ public class TestLazyIf {
     }
 
   @Test
-  public void testLazyIfWithNested() {
+  public void testLazyIfWithNestedSuccessIf() {
     Expression expression = new Expression("if(a=0,0,if(5/a>3,2,4))");
     expression.setVariable("a", new BigDecimal(0));
     Assert.assertEquals(expression.eval(),new BigDecimal(0));
   }
 
   @Test(expected = ArithmeticException.class)
-  public void testLazyIfWithNested2() {
+  public void testLazyIfWithNestedFailingIf() {
     Expression expression = new Expression("if(a=0,if(5/a>3,2,4),0)");
     expression.setVariable("a", new BigDecimal(0));
     Assert.assertEquals(expression.eval(),new BigDecimal(0));
