5c9fe968440f97c9cbcf66c386a719367a677bd5
Leonardo Soares <54752833+LeonardoSoaresDev@users.noreply.github.com>
2024-06-30 11:40:09 -0300

    adds new  function STR_TRIM (#486)

---
+++ src/main/java/com/ezylang/evalex/functions/string/StringTrimFunction.java
@@ -0,0 +1,38 @@
+/*
+  Copyright 2012-2024 Udo Klimaschewski
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+package com.ezylang.evalex.functions.string;
+
+import com.ezylang.evalex.EvaluationException;
+import com.ezylang.evalex.Expression;
+import com.ezylang.evalex.data.EvaluationValue;
+import com.ezylang.evalex.functions.AbstractFunction;
+import com.ezylang.evalex.functions.FunctionParameter;
+import com.ezylang.evalex.parser.Token;
+
+/**
+ * Returns the given string with all leading and trailing space removed.
+ *
+ * @author LeonardoSoaresDev
+ */
+@FunctionParameter(name = "string")
+public class StringTrimFunction extends AbstractFunction {
+  @Override
+  public EvaluationValue evaluate(
+      Expression expression, Token functionToken, EvaluationValue... parameterValues)
+      throws EvaluationException {
+    return expression.convertValue(parameterValues[0].getStringValue().trim());
+  }
+}
